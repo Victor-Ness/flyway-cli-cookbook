2d7b9fc7a5ee875ced14c107a5e3835fdbc536af
Victor Ness <victor@defensestorm.com>
2021-07-23 13:42:34 -0700

    update cookbook dependency

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,10 @@ description      'Installs and execute flyway cli'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/ClouDesire/flyway-cli-cookbook' if respond_to?(:source_url)
 issues_url       'https://github.com/ClouDesire/flyway-cli-cookbook/issues' if respond_to?(:issues_url)
-version          '1.0.2'
+version          '1.0.3'
 supports         'ubuntu'
 supports         'windows'
 
-depends          'java', '<= 6.0.0'
+depends          'java', '<= 7.0.0'
 depends          'ark'
 depends          'compat_resource' # For custom resource compilation on chef-client versions < 12.5
